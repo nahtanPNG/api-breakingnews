d221f7ef21d580bb32af798ac5673f2c589361dd
nahtanpng <nathan.ferreiira13@gmail.com>
2024-07-31 12:12:22 -0300

    feat: Add password cryptography

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, select: false },
   avatar: { type: String },
   background: { type: String },
+});
+
+// Antes de salvar o schema realizar a criptografia
+UserSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 // Definindo o scheema e sua variável
